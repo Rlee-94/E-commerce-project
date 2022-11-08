8836ad27622c1d76c4da1ff6e3ef91449cc26494
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-11-07 22:00:46 -0600

    Created new App and added Boostrap Navbar
    *After fighting with my code for over an hour, I was finally able to
    add a react-boostrap Navbar with a md breakpoint.

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.scss";
+import "./components/Navigation";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
