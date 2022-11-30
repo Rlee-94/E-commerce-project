520384a7c717998195ba1977b745e20cbc329e09
Reggie Lee <reg@Reggies-MacBook-Air.local>
2022-11-30 17:20:07 -0600

    added padding to form input and increased input font size.

--- my-app/src/components/Form/ModalForm.js
+++ my-app/src/components/Form/ModalForm.js
@@ -19,26 +19,45 @@ export default function ModalForm(props) {
         <Modal.Header closeButton id="modal__header">
           <Modal.Title>Get Started Today!</Modal.Title>
         </Modal.Header>
+
         <Modal.Body id="modal">
           <Form id="form">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label id="form__label">First Name</Form.Label>
-              <Form.Control type="text" placeholder="First name" autoFocus />
+              <Form.Control
+                id="form__input"
+                type="text"
+                placeholder="First name"
+                autoFocus
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label id="form__label">Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Last name" />
+              <Form.Control
+                id="form__input"
+                type="text"
+                placeholder="Last name"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label id="form__label">Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@email.com" />
+              <Form.Control
+                id="form__input"
+                type="email"
+                placeholder="name@email.com"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label id="form__label">Telephone</Form.Label>
-              <Form.Control type="tel" placeholder="(111) - 222 - 3333" />
+              <Form.Control
+                id="form__input"
+                type="tel"
+                placeholder="(111) - 222 - 3333"
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
+
         <Modal.Footer id="modal__footer">
           <Button id="modal__close" variant="secondary" onClick={handleClose}>
             Close
