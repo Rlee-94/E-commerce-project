ae8d3779fb42525a49cc24392e5aa9952bfdf334
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-12-03 14:57:23 -0600

    Started Form validation with react-hook-Form/Yup
    -Installed react-hook-form and yup for form validation.
    -Created a yup schema to validate the form inputs. The phone input
    needs a RegExp that was defined above the schema object.
    -added the {...register} function to each input to spread props into
    the input.
    -Changed modal button to a submit button.

--- my-app/src/components/Form/ModalForm.js
+++ my-app/src/components/Form/ModalForm.js
@@ -2,12 +2,33 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+const schema = Yup.object().shape({
+  firstName: Yup.string().required(),
+  lasttName: Yup.string().required(),
+  email: Yup.string().email().required(),
+  phone: Yup.string().matches(phoneRegExp, "Phone number is not valid"),
+});
 
 export default function ModalForm(props) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -22,37 +43,41 @@ export default function ModalForm(props) {
 
         <Modal.Body id="modal">
           <Form id="form">
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="firstName">
               <Form.Label id="form__label">First Name</Form.Label>
               <Form.Control
                 id="form__input"
                 type="text"
                 placeholder="First name"
                 autoFocus
+                {...register("firstName")}
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="lastName">
               <Form.Label id="form__label">Last Name</Form.Label>
               <Form.Control
                 id="form__input"
                 type="text"
                 placeholder="Last name"
+                {...register("lastName")}
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="email">
               <Form.Label id="form__label">Email address</Form.Label>
               <Form.Control
                 id="form__input"
                 type="email"
                 placeholder="name@email.com"
+                {...register("email")}
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="phone">
               <Form.Label id="form__label">Telephone</Form.Label>
               <Form.Control
                 id="form__input"
                 type="tel"
                 placeholder="(111) - 222 - 3333"
+                {...register("phone")}
               />
             </Form.Group>
           </Form>
@@ -62,7 +87,12 @@ export default function ModalForm(props) {
           <Button id="modal__close" variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button id="modal__submit" variant="primary" onClick={handleClose}>
+          <Button
+            id="modal__submit"
+            variant="primary"
+            type="submit"
+            onClick={handleSubmit(submitForm)}
+          >
             Submit
           </Button>
         </Modal.Footer>
